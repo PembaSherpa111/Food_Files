27900fbe9838dcc2de5dcc6ec6477f2d99b1dcbf
PembaSherpa111 <sherpapemba111@gmail.com>
2021-12-29 11:24:11 -0600

    this is python file for foods file 2 exercise

---
+++ Food_Files_2.py
@@ -0,0 +1,33 @@
+import json
+
+f = open('foods_info.json') #json file needs to be in same folder 
+string_dictionary = f.read()
+foods_info = json.loads(string_dictionary) 
+f.close()
+
+low_fat_count = 0
+high_fiber_count = 0
+low_glycemic_index_count = 0
+
+for i in range(0,len(foods_info)):
+    dict=foods_info[i]
+    if (dict['low fat']).lower() == 'yes':
+        low_fat_count += 1
+    if (dict['high fiber']).lower() == 'yes':
+        high_fiber_count += 1
+    if (dict['low glycemic index']).lower() == 'yes':
+        low_glycemic_index_count += 1
+
+print(f'Number of low fat foods = {low_fat_count}, Number of high fiber foods = {high_fiber_count}, and Number of low glycemic index foods = {low_glycemic_index_count}')
+
+print(f'''
+Percentage of low fat foods = {(low_fat_count/len(foods_info))*100} %
+Percentage of high fiber foods = {(high_fiber_count/len(foods_info))*100} %
+Percentage of low glycemic index foods = {(low_glycemic_index_count/len(foods_info)*100)} %\n''')
+
+for i in range(0,len(foods_info)):
+    dict=foods_info[i]
+    if (dict['low fat']).lower() == 'yes':
+        if (dict['high fiber']).lower() == 'yes':
+            if (dict['low glycemic index']).lower() == 'yes':
+                print(f'{dict["foods"]} is recommended food.')
